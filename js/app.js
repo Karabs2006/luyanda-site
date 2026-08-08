4377bff835db8209dce247d214b36b031505f879
Kayy <157222013+Karabs2006@users.noreply.github.com>
2026-08-08 14:11:31 +0200

    Animation testing
    - Added animations for section pinning and fading text in

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const galleryContainer = document.querySelector(".subsections");
 
+gsap.registerPlugin(ScrollTrigger);
+
+
 const pages = [
     {
-        name: "Jeans",
+        name: "Denim",
         image: "jeans.jpg"
     },
 
@@ -21,21 +24,60 @@ const pages = [
 pages.forEach((page) => {
 
     const card = document.createElement("div");
+
     card.classList.add("gallery-card")
 
     card.innerHTML = 
     `
-    <h1>${page.name}</h1>
+    <h1 class="subsection-name">${page.name}</h1>
 
     <button type="button" class="subsection-btn"><strong>See More</strong></button>
     `
 
+    const subsectionHeading = card.querySelector(".subsection-name");
+
     card.style.backgroundImage = `url("./Media/${page.image}")`;
 
 
+    gsap.fromTo(subsectionHeading, 
+    {
+        opacity: 0
+    },
+
+    {   opacity: 2.5,
+        duration: 10,
+        
+        
+
+        
+        scrollTrigger: {
+
+            trigger: card,
+            start: "top 20%",
+            //end: "bottom bottom",
+            scrub: true,
+
+            markers: true
+        
+        }
+
+        
+    }
+
+);
+
 galleryContainer.appendChild(card);
 
-
+ScrollTrigger.create({
+        trigger: card,
+        pin: true,
+        pinSpacing: true,
+        start: "top top",
+        end:"bottom center",
+      
+        });
 
 
 })
+
+
